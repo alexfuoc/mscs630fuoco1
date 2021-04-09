e193773b1fd58058585c530f4a9825453aed85ba
Alex Fuoco <alexfuoc@gmail.com>
2021-04-09 14:11:57 -0400

    Lab 5 with test cases updated

--- src/test/java/Driver_lab5Test.java
+++ src/test/java/Driver_lab5Test.java
@@ -1,70 +1,193 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import labs.AESCipher;
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
 
 public class Driver_lab5Test {
     String error = "An error occurred.";
-
+    
     @Test
-    void test1() { //EXAMPLE
-        String plainText = "54776F204F6E65204E696E652054776F";
-        String keyHex = "5468617473206D79204B756E67204675";
-        String cipherAnswer = "29C3505F571420F6402299B31A02D73A";
+    void test1() {
+        File input1 = new File("src/test/java/lab5data/input.5.1.in");
+        File output1 = new File("src/test/java/lab5data/input.5.1.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
     @Test
-    void test2() { //MINE
-        String plainText = "6D736373363330206165732074657374"; //mscs630 aes test
-        String keyHex = "7468697320697320746865206B657921"; //this is the key!
-        String cipherAnswer = "B639E8A9B9FC3BB7BAFAA92593CB75D6";
+    void test2() {
+        File input1 = new File("src/test/java/lab5data/input.5.2.in");
+        File output1 = new File("src/test/java/lab5data/input.5.2.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
     @Test
-    void test3() { //MINE
-        String plainText = "696E6565643136636861727368616861"; //ineed16charshaha
-        String keyHex = "2179656B206568742073692073696874"; //!yek eht si siht
-        String cipherAnswer = "CAF02C368D5B5C69B91DF799272DE82A";
+    void test3() {
+        File input1 = new File("src/test/java/lab5data/input.5.3.in");
+        File output1 = new File("src/test/java/lab5data/input.5.3.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
-
     @Test
-    void test4() { //MINE
-        String plainText = "48424B5349544557425150504F4B494E"; //HBKSITEWBQPPOKIN
-        String keyHex = "696D696E353432333862786373776139"; //imin54238bxcswa9
-        String cipherAnswer = "7752C847454B7203CE5B28BB5D33E46B";
+    void test4() {
+        File input1 = new File("src/test/java/lab5data/input.5.4.in");
+        File output1 = new File("src/test/java/lab5data/input.5.4.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
-
     @Test
     void test5() {
-        String plainText = "73736F72732C204C697A6172642C2053";
-        String keyHex = "526F636B2C2050617065722C20536369";
-        String cipherAnswer = "57D43EBAEE7EEDCD443A10FF6FE0E325";
+        File input1 = new File("src/test/java/lab5data/input.5.5.in");
+        File output1 = new File("src/test/java/lab5data/input.5.5.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
-
     @Test
     void test6() {
-        String plainText = "6E6720636F636F6E757473206D696772";
-        String keyHex = "41726520796F75207375676765737469";
-        String cipherAnswer = "036F1E119CFD4FEC53A4788C43060676";
+        File input1 = new File("src/test/java/lab5data/input.5.6.in");
+        File output1 = new File("src/test/java/lab5data/input.5.6.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
-
     @Test
     void test7() {
-        String plainText = "61746E65737320697320726573706F6E";
-        String keyHex = "546865207072696365206F6620677265";
-        String cipherAnswer = "4E8F2B5B03E235D65372BC564F734CAA";
+        File input1 = new File("src/test/java/lab5data/input.5.7.in");
+        File output1 = new File("src/test/java/lab5data/input.5.7.ans");
 
-        assertEquals(cipherAnswer, AESCipher.AES(plainText, keyHex));
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void test8() {
+        File input1 = new File("src/test/java/lab5data/input.5.8.in");
+        File output1 = new File("src/test/java/lab5data/input.5.8.ans");
+
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void test9() {
+        File input1 = new File("src/test/java/lab5data/input.5.9.in");
+        File output1 = new File("src/test/java/lab5data/input.5.9.ans");
+
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void test10() {
+        File input1 = new File("src/test/java/lab5data/input.5.10.in");
+        File output1 = new File("src/test/java/lab5data/input.5.10.ans");
+
+        try (
+            Scanner inputReader1 = new Scanner(input1);
+            Scanner outputReader1 = new Scanner(output1);
+            ){
+            String keyHex = inputReader1.nextLine();
+            String pTextHex = inputReader1.nextLine();
+            String answer = outputReader1.nextLine();
+            assertEquals(answer, AESCipher.AES(pTextHex, keyHex));
+        } catch (FileNotFoundException e) {
+            System.err.println(error);
+            e.printStackTrace();
+        }
     }
 }
